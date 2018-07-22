04e94813c9c57b4ff8fcc54302a428a439cf6233
Jacob Brady <polyfish42@gmail.com>
2018-07-22 10:19:41 -0400

    Add Movie class

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,18 +1,34 @@
 class Movie
-    MPAA_RATINGS = [
-        "G",
-        "PG",
-        "PG-13",
-        "R",
-        "NC-17"
-    ]
-
     attr_reader :title, :release_year, :mpaa_rating, :run_time
-    
+
     def initialize(title, release_year, mpaa_rating, run_time)
         @title = title
         @release_year = release_year
         @mpaa_rating = mpaa_rating
         @run_time = run_time
+    end
+
+    def start_and_end_time(time)
+        start_time = time.subtract_minutes(@run_time)
+        start_time = round_down_to_multiple_of_five(start_time)
+        end_time = start_time.add_minutes(@run_time)
+
+        [start_time, end_time]
+    end
+
+    def print_runtime
+        hour = @run_time / 60
+        h_str = hour.to_s
+        minute = @run_time % 60
+        m_str = minute.to_s.rjust(2, "0") 
+
+        "#{h_str}:#{m_str}"
+    end
+
+    private
+
+    def round_down_to_multiple_of_five(time)
+        new_minutes = time.minute - time.minute / 5 * 5
+        time.subtract_minutes(new_minutes)
     end
 end
---
+++ spec/movie_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require 'movie.rb'
+require 'schedule_time'
+
+describe Movie do
+    subject(:movie) { Movie.new("There's Something About Mary", 1998, "R", 134)}
+    let(:time1) {ScheduleTime.new(18,14)}
+    let(:time2) {ScheduleTime.new(18,0)}
+
+    describe "#start_and_end_time" do
+        it "pick a start and end time" do
+            (start_time, end_time) = movie.start_and_end_time(time1)
+
+            expect(start_time.to_s).to eq "16:00"
+            expect(end_time.to_s).to eq "18:14"
+        end
+
+        it "picks start time in intervals of five minutes" do
+            (start_time, end_time) = movie.start_and_end_time(time2)
+
+            expect(start_time.to_s).to eq "15:45"
+            expect(end_time.to_s).to eq "17:59"
+        end
+    end
+
+    describe "#print_runtime" do
+        it "prints movie run times over an hour" do
+            expect(movie.print_runtime).to eq "2:14"
+        end
+    end
+end
